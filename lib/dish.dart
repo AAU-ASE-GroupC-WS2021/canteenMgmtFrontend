0ba3ed3e31486d6695cd0dc9547f2b0051539bac
Elias <elias.santner@edu.htl-villach.at>
2022-01-09 19:20:01 +0100

    tried to implement https request

---
+++ lib/dish.dart
@@ -0,0 +1,22 @@
+class Dish {
+  final int id;
+  final String name;
+  final double price;
+  final String type;
+
+  Dish({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.type,
+  });
+
+  factory Dish.fromJson(Map<String, dynamic> json) {
+    return Dish(
+      id: json['id'],
+      name: json['name'],
+      price: json['price'],
+      type: json['type'],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:html';
+import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:http/http.dart' as http;
+
+import 'dish.dart';
 import 'qr_demo.dart';
+
+
 
 void main() {
   runApp(const MyApp());
@@ -31,6 +42,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String _result = '';
+  late Future<List<Dish>> futureDishes;
+
+  @override
+  void initState() {
+    super.initState();
+    futureDishes = fetchDishes();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,9 +78,43 @@ class _MyHomePageState extends State<MyHomePage> {
               _result,
               style: Theme.of(context).textTheme.headline4,
             ),
-          ],
+
+            FutureBuilder<List<Dish>>(
+              future: futureDishes,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data!.toString());
+                } else if (snapshot.hasError) {
+                  print(snapshot.stackTrace);
+                  return Text('${snapshot.error}');
+                }
+
+                // By default, show a loading spinner.
+                return const CircularProgressIndicator();
+              },
+            ),],
         ),
       ),
     );
+  }
+}
+
+Future<List<Dish>> fetchDishes() async {
+  var client = http.Client();
+  final response = await client.get(Uri.parse('https://localhost:8443/dish'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    final stringData = response.body;
+    var responseJson = json.decode(stringData);
+    // TODO: might not work - can not really test while http request fails
+    return (responseJson as List)
+        .map((p) => Dish.fromJson(p))
+        .toList();
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load dishes');
   }
 }
