8d61e67da81bd0c77ea8493eae3d6cca4d039dea
emgo_xxq <31079238+emirgoran@users.noreply.github.com>
2022-01-27 00:04:58 +0100

    Validate username and password on form

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -26,12 +26,17 @@ class _SignupScreenState extends State<SignupScreen> {
     existingProfiles = signupService.fetchDishes();
   }
 
+  final _formKey = GlobalKey<FormState>();
+
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Form(
+        key: _formKey,
+        child:
+        Scaffold(
       appBar: AppBar(title: const Text('Create a new profile')),
       body: Center(
         child: Column(
@@ -41,16 +46,22 @@ class _SignupScreenState extends State<SignupScreen> {
                 width: 300.0,
                 child: Column(
                   children: [
-                    TextField(
+                    TextFormField(
                       decoration: const InputDecoration(
                         labelText: 'Username',
                       ),
                       controller: usernameController,
+                      validator: (value) {
+                        return validateUsername(value);
+                      },
                     ),
                     const SizedBox(height: 20), // space between buttons
-                    TextField(
+                    TextFormField(
                       obscureText: true,
                       controller: passwordController,
+                      validator: (value) {
+                        return validatePassword(value);
+                      },
                       decoration: const InputDecoration(
                         labelText: 'Password',
                       ),
@@ -58,18 +69,20 @@ class _SignupScreenState extends State<SignupScreen> {
                     const SizedBox(height: 20), // space between buttons
                     ElevatedButton(
                       onPressed: () => {
-                      signupService.createProfile(usernameController.text, passwordController.text)
-                        .then((value) => {
-                        if (value) {
-                          context.beamToNamed('/signup-finished')
-                        }
-                        else {
-                          showAlertDialog("Could not create profile!")
-                        }
-                      })
-                        .onError((error, stackTrace) => {
-                        showAlertDialog("Unknown error occured while trying to create a new profile.\nPlease refresh the page and try again.!")
-                      })
+                      if (_formKey.currentState!.validate()) {
+                        signupService.createProfile(usernameController.text, passwordController.text)
+                            .then((value) => {
+                          if (value) {
+                            context.beamToNamed('/signup-finished')
+                          }
+                          else {
+                            showAlertDialog("Could not create profile!")
+                          }
+                        })
+                            .onError((error, stackTrace) => {
+                          showAlertDialog("Unknown error occured while trying to create a new profile.\nPlease refresh the page and try again.!")
+                        })
+                      }
                       },
                       child: const Text('Sign up'),
                     ),
@@ -79,6 +92,7 @@ class _SignupScreenState extends State<SignupScreen> {
           ],
         ),
       ),
+    ),
     );
   }
 
@@ -107,4 +121,51 @@ class _SignupScreenState extends State<SignupScreen> {
       },
     );
   }
+
+  static final regexAtLeastOneUppercaseLeter = RegExp(r'(?=.*[A-Z])');
+  static final regexAtLeastOneLowercaseLeter = RegExp(r'(?=.*[a-z])');
+  static final regexAtLeastOneDigit = RegExp(r'(?=.*[0-9])');
+  static final regexAlphanumericOnly = RegExp(r'^[a-zA-Z0-9]+$');
+
+  String? validatePassword(String? password) {
+    if (password == null || password.isEmpty) {
+      return 'Password field cannot be empty.';
+    }
+    else if (password.length < 9) {
+      return 'Password has to be at least 9 characters long.';
+    }
+    else if (password.length > 64) {
+      return 'Password cannot be longer than 64 characters.';
+    }
+    else if (!regexAtLeastOneUppercaseLeter.hasMatch(password)) {
+      return 'Password must contain at least one uppercase letter.';
+    }
+    else if (!regexAtLeastOneLowercaseLeter.hasMatch(password)) {
+      return 'Password must contain at least one lowercase letter.';
+    }
+    else if (!regexAtLeastOneDigit.hasMatch(password)) {
+      return 'Password must contain at least one digit.';
+    }
+
+    return null;
+  }
+
+  String? validateUsername(String? username) {
+    if (username == null || username.isEmpty) {
+      return 'Username field cannot be empty.';
+    }
+    else if (username.length < 3) {
+      return 'Username has to be at least 3 characters long.';
+    }
+    else if (username.length > 24) {
+      return 'Username cannot be longer than 24 characters.';
+    }
+    else if (!regexAlphanumericOnly.hasMatch(username)) {
+      return 'Username can contain only letters and digits.';
+    }
+
+    return null;
+  }
 }
+
+
