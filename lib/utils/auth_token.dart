fe6042f8b940f2824e1e9acf64dc033e71ef02d3
emgo_xxq <31079238+emirgoran@users.noreply.github.com>
2022-01-29 00:22:22 +0100

    Use cookies instead of SharedPreferences
    SharedPreferences do not work if you even refresh the page

--- lib/utils/auth_token.dart
+++ lib/utils/auth_token.dart
@@ -1,4 +1,6 @@
-import 'package:shared_preferences/shared_preferences.dart';
+library utils.authTokenUtils;
+
+import 'dart:html' as html;
 
 class AuthTokenUtils {
 
@@ -12,14 +14,44 @@ class AuthTokenUtils {
     defaultValue: '36',
   );
 
+  static bool? _loggedIn = null;
+
   static Future<String?> getAuthToken() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getString(authTokenKey);
+    if (!isLoggedIn()) {
+      return null;
+    }
+
+    return getCookie(authTokenKey);
   }
 
   static void setAuthToken(String token) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString(authTokenKey, token);
+    html.window.document.cookie = "$authTokenKey=$token;";
+    _loggedIn = token.length.toString() == authTokenLength;
+  }
+
+  static bool isLoggedIn() {
+    if (_loggedIn == null) {
+      return getCookie(authTokenKey).isNotEmpty;
+    }
+
+    return _loggedIn == true;
+  }
+
+  static String getCookie(String key) {
+
+    String cookies = html.window.document.cookie ?? "";
+    List<String> listValues = cookies.isNotEmpty ? cookies.split(";") : [];
+    String matchVal = "";
+    for (int i = 0; i < listValues.length; i++) {
+      List<String> map = listValues[i].split("=");
+      String _key = map[0].trim();
+      String _val = map[1].trim();
+      if (key == _key) {
+        matchVal = _val;
+        break;
+      }
+    }
+    return matchVal;
   }
 
 }
