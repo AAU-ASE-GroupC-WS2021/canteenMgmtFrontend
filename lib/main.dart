83de1e21b2b35e8604b5f7998105d162cf0386a5
Max Jakobitsch <minigugler@gmail.com>
2022-02-06 23:38:45 +0100

    refactor: extract beamer (navigation) config
    getting too big; clutters up main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'beamer_locations.dart';
 import 'cubits/auth.dart';
-import 'screens/dish_service_demo.dart';
-import 'screens/home.dart';
-import 'screens/qr_demo.dart';
-import 'screens/qr_scanner.dart';
-import 'screens/signin_screen.dart';
-import 'screens/signup_finished.dart';
-import 'screens/signup_screen.dart';
 import 'services/dish_service.dart';
 import 'services/key_value_shared_prefs.dart';
 import 'services/key_value_store.dart';
@@ -38,41 +32,7 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
-  final beamerDelegate = BeamerDelegate(
-    locationBuilder: RoutesLocationBuilder(
-      routes: {
-        // Return either Widgets or BeamPages if more customization is needed
-        '/': (context, state, data) => const BeamPage(
-              title: 'Canteen Management',
-              child: HomeScreen(),
-            ),
-        '/dish': (context, state, data) => const BeamPage(
-              title: 'Dish Demo',
-              child: DishDemoScreen(),
-            ),
-        '/qr-demo': (context, state, data) => BeamPage(
-              title: 'QR Scanner Demo',
-              child: QrDemoScreen(scanValue: data is String? ? data : null),
-            ),
-        '/qr-scan': (context, state, data) => const BeamPage(
-              title: 'Scan QR Code',
-              child: QrScannerScreen(),
-            ),
-        '/signup': (context, state, data) => const BeamPage(
-              title: 'Create a new profile',
-              child: SignupScreen(),
-            ),
-        '/signin': (context, state, data) => const BeamPage(
-              title: 'Log in',
-              child: SignInScreen(),
-            ),
-        '/signup/finished': (context, state, data) => const BeamPage(
-              title: 'Profile creation confirmation',
-              child: SignupFinishedScreen(),
-            ),
-      },
-    ),
-  );
+  final beamerDelegate = getBeamerDelegate();
 
   @override
   Widget build(BuildContext context) {
