45af6d92c84599f443ab26203d768d8c3e6ce483
Max Jakobitsch <minigugler@gmail.com>
2022-02-08 00:15:26 +0100

    test: update demo tests

--- test/demo_test.dart
+++ test/demo_test.dart
@@ -9,13 +9,16 @@ import 'package:canteen_mgmt_frontend/main.dart';
 import 'package:canteen_mgmt_frontend/models/dish.dart';
 import 'package:canteen_mgmt_frontend/services/dish_service.dart';
 import 'package:canteen_mgmt_frontend/services/key_value_store.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:http/http.dart' as http;
 
 import 'demo_test.mocks.dart'; // if this fails run build_runner (see readme)
+
+const _exampleDish = Dish(name: 'Some Test Dish', price: 123, type: 'MAIN');
 
 // generate a mocked version of DishService
 // to actually generate the .mocks.dart file, run `flutter pub run build_runner build --delete-conflicting-outputs`
@@ -33,18 +36,19 @@ void main() {
     // set up stub for fetchDishes()
     // since the service works asynchronously (because of networking),
     // using ".thenAnswer" and wrapping the result in a Future is necessary
-    when(dishService.fetchDishes()).thenAnswer((_) => Future.value(const [
-          Dish(
-            name: "Test Dish 1",
-            price: 9.99,
-            type: "STARTER",
-          ),
-          Dish(
-            name: "Test Dish 2",
-            price: 10,
-            type: "MAIN",
-          ),
-        ]));
+    when(dishService.fetchDishes(dishDay: anyNamed('dishDay')))
+        .thenAnswer((_) => Future.value(const [
+              Dish(
+                name: "Test Dish 1",
+                price: 9.99,
+                type: "STARTER",
+              ),
+              Dish(
+                name: "Test Dish 2",
+                price: 10,
+                type: "MAIN",
+              ),
+            ]));
 
     // set up stub for createDish (this is required, even if we are not interested in the value)
     // as a return value we just use the input
@@ -75,8 +79,25 @@ void main() {
     await tester.tap(find.text('Create Dish'));
     await tester.pumpAndSettle();
 
+    var textField = find.byType(TextFormField);
+    await tester.enterText(textField.at(0), _exampleDish.name);
+    await tester.enterText(textField.at(1), '${_exampleDish.price}');
+
+    await tester.tap(find.text('NOMENUDAY').hitTestable());
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('MONDAY').hitTestable());
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('Create Dish').hitTestable().last);
+    await tester.pumpAndSettle();
+
     // but after pressing the button, we do expect that the method was called
     verify(dishService.createDish(any));
+
+    await tester.tap(find.text('Refresh'));
+    await tester.pumpAndSettle();
+
+    // expect(find.text(_exampleDish.name), findsWidgets);
 
     expect(find.text('This text appears nowhere in the app!'), findsNothing);
   });
